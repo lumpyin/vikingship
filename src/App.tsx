2c36336742b50ede4dd4e23d94dd37c99498f920
zyz <zyzisgood@gmail.com>
2020-04-27 21:50:30 -0500

    update base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 const App:React.FC =()=> {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <h1>Hello world</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
