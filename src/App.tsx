985a74f857454a40e6e0842accc12b10bd4147f4
zyz <zyzisgood@gmail.com>
2020-05-07 22:52:05 -0500

    submenu update

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@ import React from 'react';
 import Button,{ButtonType,ButtonSize} from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
+import SubMenu from './components/Menu/subMenu';
+
 
 const App:React.FC =()=> {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0}>
-          <MenuItem index={0}>
+        <Menu defaultIndex={0} mode='vertiacal'>
+          <MenuItem >
             cool link
           </MenuItem>
-          <MenuItem index={1}>
+          <MenuItem disabled>
             cool link2
           </MenuItem>
-          <MenuItem index={2}>
-            cool link3
+          <SubMenu title="dropdown">
+            <MenuItem >
+              dropdown 1
+            </MenuItem>
+            <MenuItem >
+              dropdown 2
+            </MenuItem>
+          </SubMenu>
+          <MenuItem >
+              cool link3
           </MenuItem>
         </Menu>
         <Button autoFocus>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
-        <Button btnType={ButtonType.Link} href="http://www.google.com">google</Button>
+       
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -2,3 +2,46 @@ import React,{ useContext, useState, FunctionComponentElement } from 'react'
 import classNames from 'classnames'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem'
+
+export interface SubMenuProps {
+    index?:number;
+    title:string;
+    className?:string;
+}
+
+const SubMenu:React.FC<SubMenuProps> = ({index,title,children,className}) => {
+    const context = useContext(MenuContext);
+    const classes = classNames('menu-item submenu-',className,{
+        'is-active':context.index === index
+    })
+
+    const renderChildren = ()=> {
+        const childrenComponent = React.Children.map(children,(child,i)=> {
+            const childElement = child as FunctionComponentElement<MenuItemProps>
+            if(childElement.type.displayName === 'MenuItem'){
+                return childElement;
+            }else{
+                console.error('error');
+            }
+        })
+
+        return (
+            <ul className='viking-submenu'>
+                {childrenComponent}
+            </ul>
+        )
+    }
+
+
+    return (
+        <li key={index} className={classes}>
+            <div className="submenu-title">
+              {title}
+            </div>
+            {renderChildren()}
+        </li>
+    )
+}
+
+SubMenu.displayName = 'SubMenu';
+export default SubMenu;
